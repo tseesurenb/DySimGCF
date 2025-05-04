70443116d524971af2637511bc704ca52e3108b7
tseesurenb <tseesuren@gmail.com>
2025-05-04 13:24:28 +1000

    hi

--- parse.py
+++ parse.py
@@ -34,10 +34,10 @@ def parse_args():
     parser.add_argument('--weighted_neg_sampling', type=bool, default=False)
     parser.add_argument('--samples', type=int, default=1)
     parser.add_argument('--load', type=bool, default=False)
-    parser.add_argument('--save_res', type=bool, default=True)
-    parser.add_argument('--save_pred', type=bool, default=False)
-    parser.add_argument('--save_model', type=bool, default=False)
-    parser.add_argument('--save_sim_mat', type=bool, default=False)
+    parser.add_argument('--save_res', action='store_true')
+    parser.add_argument('--save_pred', action='store_true')
+    parser.add_argument('--save_model', action='store_true')
+    parser.add_argument('--save_sim_mat', action='store_true')
     parser.add_argument('--margin', type=float, default=0.0, help="the margin in BPR loss")
 
      # Diffusion/ODE settings
